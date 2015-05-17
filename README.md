f60fee8e35570540300c1944b4988cb23104ef43
Herrhelms <dev@straw.io>
2015-05-17 23:41:17 +0000

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # meteor-mailchimp-3
 Use MailChimp API 3.0 via meteor
+
+### this package is a placeholder for now. you can find it on [Atmospherejs.com](https://atmospherejs.com/herrhelms) 
+
+### what's inside 
+
+Custom 
--- package.js
+++ package.js
@@ -1,22 +1,24 @@
 Package.describe({
-  name: 'herrhelms:meteor-mailchimp-3',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'herrhelms:meteor-mailchimp-3',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: '',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/herrhelms/meteor-mailchimp-3.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('meteor-mailchimp-3.js');
+    api.versionsFrom('1.1.0.2');
+    api.addFiles('meteor-mailchimp-3.js');
+    api.addFiles(['client/mailchimp.js'], 'client');
+    api.addFiles(['server/mailchimp.js'], 'server');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('herrhelms:meteor-mailchimp-3');
-  api.addFiles('meteor-mailchimp-3-tests.js');
+    api.use('tinytest');
+    api.use('herrhelms:meteor-mailchimp-3');
+    api.addFiles('meteor-mailchimp-3-tests.js');
 });
